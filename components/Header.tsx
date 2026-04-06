2d097a0f541d56ddf9ae2bd31f60eb6ee34081e8
Yash Vashishth <imyashvashishth05@gmail.com>
2026-04-06 23:44:13 +0530

    Implement Search Stocks

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,17 @@ import React from 'react'
 import logo from '../public/public/assets/icons/logo.svg'
 import NavItems from './NavItems'
 import UserDropdown from './UserDropdown'
+import { searchStocks } from '@/lib/actions/finnhub.actions'
+import { auth } from '@/lib/better-auth/auth'
+import { headers } from 'next/headers'
 
-const Header = () => {
+const Header = async () => {
+  const initialStocks = await searchStocks();
+  const session = await auth.api.getSession({ headers: await headers() });
+  const user = session?.user ? { id: session.user.id, name: session.user.name, email: session.user.email } : null;
+
+  if (!user) return null;
+
   return (
     <header className='sticky top-0 header'>
       <div className='container header-wrapper'>
@@ -13,9 +22,9 @@ const Header = () => {
           <Image src={logo} alt='Signalist logo' width={140} height={32} className='h-8 auto cursor-pointer' />
         </Link>
         <nav className='hidden sm:block'>
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
-        <UserDropdown />
+        <UserDropdown user={user} initialStocks={initialStocks} />
       </div>
     </header>
   )
