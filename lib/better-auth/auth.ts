33ae71d43c3e835fa4d9fd73b155b201a27dd26f
Yash Vashishth <imyashvashishth05@gmail.com>
2026-04-09 20:27:28 +0530

    auth.ts changes again

--- lib/better-auth/auth.ts
+++ lib/better-auth/auth.ts
@@ -3,45 +3,32 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { connectToDatabase } from "@/database/mongoose";
 import { nextCookies } from "better-auth/next-js";
 
-let authPromise: ReturnType<typeof initAuth> | null = null;
+const mongoose = await connectToDatabase();
+const client = mongoose?.connection.getClient() as any;
+const db = client?.db('user');
 
-async function initAuth() {
-    const mongoose = await connectToDatabase();
-    const client = mongoose?.connection.getClient() as any;
-    const db = client?.db('user');
+if (!db) throw new Error('MongoDB connection not found');
 
-    if (!db) throw new Error('MongoDB connection not found');
-
-    return betterAuth({
-        database: mongodbAdapter(db),
-        secret: process.env.BETTER_AUTH_SECRET,
-        baseURL: process.env.BETTER_AUTH_URL,
-        user: {
-            modelName: "signindata",
-            additionalFields: {
-                country: { type: "string", required: false },
-                investmentGoals: { type: "string", required: false },
-                riskTolerance: { type: "string", required: false },
-                preferredIndustry: { type: "string", required: false }
-            }
-        },
-        emailAndPassword: {
-            enabled: true,
-            disableSignUp: false,
-            requireEmailVerification: false,
-            minPasswordLength: 8,
-            maxPasswordLength: 128,
-            autoSignIn: true,
-        },
-        plugins: [nextCookies()],
-    });
-}
-
-export const getAuth = () => {
-    if (!authPromise) {
-        authPromise = initAuth();
-    }
-    return authPromise;
-}
-
-export const auth = await getAuth();
+export const auth = betterAuth({
+    database: mongodbAdapter(db),
+    secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL,
+    user: {
+        modelName: "signindata",
+        additionalFields: {
+            country: { type: "string", required: false },
+            investmentGoals: { type: "string", required: false },
+            riskTolerance: { type: "string", required: false },
+            preferredIndustry: { type: "string", required: false }
+        }
+    },
+    emailAndPassword: {
+        enabled: true,
+        disableSignUp: false,
+        requireEmailVerification: false,
+        minPasswordLength: 8,
+        maxPasswordLength: 128,
+        autoSignIn: true,
+    },
+    plugins: [nextCookies()],
+});
