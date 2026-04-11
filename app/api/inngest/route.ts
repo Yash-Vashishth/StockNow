b446768fbbb5f672eb6d69ff678fd704d8a1dbbd
Yash Vashishth <imyashvashishth05@gmail.com>
2026-04-11 15:20:21 +0530

    route.ts for inngest again

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,8 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 
-export const runtime = 'edge'; 
-
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [sendSignUpEmail , sendDailyNewsSummary]
