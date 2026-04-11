af26df6a05861945d606495784c6484cc97d4b98
Yash Vashishth <imyashvashishth05@gmail.com>
2026-04-11 16:49:42 +0530

    route.ts again but with loggin functions

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,10 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 
+console.log('Functions loaded:', { sendSignUpEmail: !!sendSignUpEmail, sendDailyNewsSummary: !!sendDailyNewsSummary });
+console.log('Signing key:', !!process.env.INNGEST_SIGNING_KEY ? 'present' : 'MISSING');
+console.log('Client ID:', inngest.id);
+
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [sendSignUpEmail , sendDailyNewsSummary]
